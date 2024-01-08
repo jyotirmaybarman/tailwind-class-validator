19fe34ffb4e33fd88aaab582669324e0589e7a72
Jyotirmay Barman <jyotirmay@developerzilla.com>
2024-01-08 14:35:09 +0530

    add: tailwind class validation logic

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello world");
+import tailwindcss from 'tailwindcss';
+import postcss from 'postcss';
+import typography from "@tailwindcss/typography"
+import autoprefixer from 'autoprefixer';
+
+export type Plugins = {
+    typography: boolean
+}
+
+export const isValidTailwindClass = async (str: string, plugins?:Plugins): Promise<boolean> => {
+    const css = `.cls{@apply ${str}}`;
+
+    const processor = postcss([
+        tailwindcss({
+            darkMode: "class",
+            content: ["*"],
+            theme: {
+                extend: {},
+            },
+            plugins: [
+                plugins?.typography ? typography : undefined
+            ],
+        }),
+        autoprefixer,
+    ]);
+
+    try {
+        await processor.process(css, { from: undefined })
+        return true
+    } catch (error) {        
+        return false
+    }
+}
